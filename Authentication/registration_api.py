c02e8170af5f9cdf6ecfe0d5dbd1bb5500d74ae3
Xinye L <118423908+xlistudent@users.noreply.github.com>
2023-03-09 17:04:40 -0800

    Update registration_api.py
    Connect to the database V1

--- Authentication/registration_api.py
+++ Authentication/registration_api.py
@@ -2,25 +2,40 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# create an empty dictionary to store user data
-users = {}
+# Database configuration
+app.config['MYSQL_USER'] = 'cs595'
+app.config['MYSQL_PASSWORD'] = 'hAlp3yGyMmZLog8S'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_DB'] = 'cs595'
 
+# Initialize the MySQL extension
+mysql = MySQL(app)
+
+# Registration API endpoint
 @app.route('/register', methods=['POST'])
 def register():
     # get the user data from the request body
     data = request.get_json()
 
-    # check if username already exists in the users dictionary
-    if data['username'] in users:
+    # check if username already exists in the database
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users WHERE username = %s", (data['username'],))
+    result = cur.fetchone()
+    cur.close()
+    
+    if result is not None:
         return jsonify({'success': False, 'message': 'Username already exists.'}), 409
 
-    # if username doesn't exist, add the user to the users dictionary
-    users[data['username']] = data['password']
+    # add the user to the database
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (data['username'], data['password']))
+    mysql.connection.commit()
+    cur.close()
 
     # return a success response
     return jsonify({'success': True, 'message': 'User registered successfully.'}), 201
 
-# test endpoint
+# Test endpoint
 @app.route('/')
 def rgTest():
     return "Registration Page"

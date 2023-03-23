ecacc0e5474b648df9c284c020235dcfe5263c2c
Kris Vesselee <81246356+krissie-94@users.noreply.github.com>
2023-03-23 16:43:44 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import './reusesablewidget/reusesablewidget.dart';
-import './widgets/welcome.dart';
+import './takequiz.dart';
 import './widgets/signin.dart';
+import './widgets/newsignin.dart';
 import './widgets/signup.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/gestures.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +15,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
       ),
       home: const MyHomePage(title: 'Applicant Assessment Test'),
     );
@@ -25,6 +28,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  // final Function()? onTap;
   const MyHomePage({super.key, required this.title});
 
   final String title;
@@ -32,6 +36,8 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
@@ -45,86 +51,91 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
+        // ignore: sized_box_for_whitespace
         body: Container(
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: Opacity(
-                  opacity: 0.3,
-                  child: Image.asset('assets/images/test_avatar.png',
-                      fit: BoxFit.cover),
-                ),
-              ),
-              Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Center(
-                        child: ClipOval(
-                            child: Container(
-                          width: 180,
-                          height: 180,
-                          // color: Color(0xFF80C038),
-                          alignment: Alignment.center,
-                          child: (Image.asset(
-                            "assets/images/project_logo.png",
-                          )),
-                        )),
-                      ),
-                      // ignore: prefer_const_constructors
-                      SizedBox(height: 50),
-                      // ignore: prefer_const_constructors
-                      Text(
-                        'Welcome',
-                        textAlign: TextAlign.center,
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      // ignore: prefer_const_constructors
-                      SizedBox(height: 50),
-                      // ignore: prefer_const_constructors
-                      Text('AAT is here for you.\n Explore Now',
-                          textAlign: TextAlign.center,
-                          // ignore: prefer_const_constructors
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18)),
-
-                      // ignore: prefer_const_constructors
-                      // SizedBox(
-                      //   height: 40,
-                      // ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: signInsignUpButton(context, "REGISTER", () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  // ignore: prefer_const_constructors
-                                  builder: (context) => SignUp()));
-                        }),
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: signInsignUpButton(context, "LOGIN", () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    // ignore: prefer_const_constructors
-                                    builder: (context) => SignIn()));
-                          })),
-                    ]),
-              )
-            ],
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Opacity(
+              opacity: 0.3,
+              child: Image.asset('assets/images/test_avatar.png',
+                  fit: BoxFit.cover),
+            ),
           ),
-        ));
+          Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Center(
+                    child: ClipOval(
+                        child: Container(
+                      width: 180,
+                      height: 180,
+                      // color: Color(0xFF80C038),
+                      alignment: Alignment.center,
+                      child: (Image.asset(
+                        "assets/images/project_logo.png",
+                      )),
+                    )),
+                  ),
+                  // ignore: prefer_const_constructors
+                  SizedBox(height: 50),
+                  // ignore: prefer_const_constructors
+                  Text(
+                    'Welcome',
+                    textAlign: TextAlign.center,
+                    // ignore: prefer_const_constructors
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  // ignore: prefer_const_constructors
+                  SizedBox(height: 50),
+                  // ignore: prefer_const_constructors
+                  Text('AAT is here for you.\n Explore Now',
+                      textAlign: TextAlign.center,
+                      // ignore: prefer_const_constructors
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18)),
+
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: signInsignUpButton(context, "LOGIN", () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                // ignore: prefer_const_constructors
+                                builder: (context) => SignIn()));
+                      })),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: signInsignUpButton(context, "SIGNUP", () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                // ignore: prefer_const_constructors
+                                builder: (context) => SignUp()));
+                      })),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: signInsignUpButton(context, "Take Quiz", () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                // ignore: prefer_const_constructors
+                                builder: (context) => TakeQuizPage()));
+                      })),
+                ]),
+          )
+        ],
+      ),
+    ));
   }
 }
+
